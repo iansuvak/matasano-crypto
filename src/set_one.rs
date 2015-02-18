094b49085581d430eef64e07d8b1df82e687ef9d
Ian Suvak <ian.suvak@gmail.com>
2015-02-17 21:58:56 -0500

    propagate error rather than throwing away

--- src/set_one.rs
+++ src/set_one.rs
@@ -1,13 +1,11 @@
 // Implementing each set as a library module and each function solution individually
 
-use rustc_serialize::hex::FromHex;
+use rustc_serialize::hex::{FromHex, FromHexError};
 use rustc_serialize::base64::{ToBase64, STANDARD};
 
-pub fn hex_to_base64(input: &str) -> Result<String, &str> {
-    let config = STANDARD;
+pub fn hex_to_base64(input: &str) -> Result<String, FromHexError> {
     match input.from_hex() {
-        Ok(hex) => Ok(hex.to_base64(config)),
-        // TODO don't throw away the Hex error
-        Err(_) => Err("Hex conversion failed"),
+        Ok(hex) => Ok(hex.to_base64(STANDARD)),
+        Err(e) => Err(e),
     }
 }
