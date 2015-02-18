66bcdf836d0ffb2d8e5f05f4e982d2a353d6fac5
Ian Suvak <ian.suvak@gmail.com>
2015-02-17 22:37:31 -0500

    implement fixed_xor

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,19 @@ pub mod set_one;
 
 #[cfg(test)]
 mod tests {
-    use set_one::hex_to_base64;
+    use set_one::{hex_to_base64, fixed_xor};
 
     #[test]
     fn test_hex_to_base64() {
         assert_eq!(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap().as_slice(),
         "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
+    }
+
+    #[test]
+    fn test_fixed_xor() {
+        assert_eq!(fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965").unwrap().as_slice(),
+        "746865206b696420646f6e277420706c6179"
         );
     }
 }
--- src/set_one.rs
+++ src/set_one.rs
@@ -1,6 +1,6 @@
 // Implementing each set as a library module and each function solution individually
 
-use rustc_serialize::hex::{FromHex, FromHexError};
+use rustc_serialize::hex::{FromHex, FromHexError, ToHex};
 use rustc_serialize::base64::{ToBase64, STANDARD};
 
 pub fn hex_to_base64(input: &str) -> Result<String, FromHexError> {
@@ -8,4 +8,14 @@ pub fn hex_to_base64(input: &str) -> Result<String, FromHexError> {
         Ok(hex) => Ok(hex.to_base64(STANDARD)),
         Err(e) => Err(e),
     }
+}
+
+pub fn fixed_xor(input_a: &str, input_b: &str) -> Result<String, FromHexError> {
+    let a = try!(input_a.from_hex());
+    let b = try!(input_b.from_hex());
+    let xored_vec: Vec<u8> = a.iter()
+        .zip(b.iter())
+        .map(|(x, y)| x ^ y)
+        .collect();
+    Ok(xored_vec.to_hex())
 }
